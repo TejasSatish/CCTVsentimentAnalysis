bb8383dac639d5889fe1689728addfcd7f1edcc3
TejasSatish <tejassatish@gmail.com>
2022-09-15 22:25:25 +0530

    can detect emotion

--- emotion_RCNN.py
+++
@@ -1,4 +0,0 @@
-import cv2
-import deepface
-import numpy as np
-import matplotlib.pyplot as plt
--- haarScan.py
+++ haarScan.py
@@ -1,48 +1,61 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import os
 
-# img1=cv2.imread('penguin.jpeg')
-# img2=cv2.imread('space.jpeg')
-# img3=cv2.imread('illuminati.jpeg')
-
-# #applying a threshold
-
-# rows,columns,channels=img3.shape
-# roi=img2[0:rows,0:columns]
-
-# img3gray=cv2.cvtColor(img3,cv2.COLOR_BGR2GRAY)
-# ret, mask=cv2.threshold(img3gray,180,255,cv2.THRESH_BINARY_INV)
-
-# mask_inv=cv2.bitwise_not(mask) #low lvl logical op
-
-# img2_bg=cv2.bitwise_and(roi,roi,mask=mask_inv)
-# img3_fg=cv2.bitwise_and(img3,img3,mask=mask)
-
-# dst=cv2.add(img2_bg,img3_fg)
-# img1[0:rows,0:columns]=dst
-
-
-# cv2.imshow('mask',mask)
-# cv2.imshow('mask_inv',mask_inv)
-# cv2.imshow('img3_fg',img3_fg)
-# cv2.imshow('img2_bg',img2_bg)
-# cv2.imshow('final',img1)
 
 cap=cv2.VideoCapture(0)
 
-frontalFace_casc=cv2.CascadeClassifier("haarcascade_frontalface.xml")
-profileFace_casc=cv2.CascadeClassifier("haarcascade_profileface.xml")
+classifier =load_model('assets\model.h5')
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+
+frontalFace_casc=cv2.CascadeClassifier("assets\haarcascade_frontalface.xml")
+profileFace_casc=cv2.CascadeClassifier("assets\haarcascade_profileface.xml")
+
+pflag=0
+fflag=0
 while True:
     ret, img=cap.read()
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     Pfaces=profileFace_casc.detectMultiScale(gray,1.3,5)
     Ffaces=frontalFace_casc.detectMultiScale(gray,1.3,5)
     for(px,py,pw,ph) in Pfaces:
-        cv2.rectangle(img,(px,py),(px+pw,py+ph),(255,0,0),2)
-    for(fx,fy,fw,fh) in Ffaces:
-        cv2.rectangle(img,(fx,fy),(fx+fw,fy+fh),(255,0,0),2)
-    cv2.imshow('img',img)
+        pflag=1
+
+        cv2.rectangle(gray,(px,py),(px+pw,py+ph),(255,0,0),2)
+
+        roi_gray = gray[py:py+ph,px:px+pw]
+        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+        if np.sum([roi_gray])!=0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0)
+
+            prediction = classifier.predict(roi)[0]
+            label=emotion_labels[prediction.argmax()]
+            label_position = (px,py)
+            cv2.putText(gray,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2) 
+    else:
+        for(fx,fy,fw,fh) in Ffaces:
+            cv2.rectangle(gray,(fx,fy),(fx+fw,fy+fh),(255,0,0),2)
+
+            roi_gray = gray[fy:fy+fh,fx:fx+fw]
+            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray])!=0:
+                roi = roi_gray.astype('float')/255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi,axis=0)
+
+                prediction = classifier.predict(roi)[0]
+                label=emotion_labels[prediction.argmax()]
+                label_position = (fx,fy)
+                cv2.putText(gray,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2) 
+
+    cv2.imshow('footage',gray)
     k=cv2.waitKey(30) & 0xff
     if k==27:
         break
